f66e0a372fb07c859be40e5c234007c8fe0bb43b
JacquelleR <jrowell@hawk.iit.edu>
2017-03-05 23:52:52 -0600

    Login Page
    Right now this is also the landing page. Need to finish the basica
    landing page

---
+++ WebPages/index.php
@@ -0,0 +1,51 @@
+<?php include 'includes/header.php'; ?>
+<body>
+<div class="Head">
+    <h1>Hawkstagram</h1>
+</div>
+<div class="overlay">
+    <div class="container">
+        <div id="boxes" class="col">
+            <div class="IITSignIn form-box">
+                <form action="_register.php" method="post">
+                    <div class="text-center">
+                        <h2>Illinois Tech Login</h2>
+                    </div>
+                    <div class="form-group">
+                        <!-- <label for="username" class="control-label">Username</label> -->
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
+                    </div>
+                    <div class="form-group">
+                        <!-- <label for="Password" class="control-label">Username</label> -->
+                        <input type="password" class="form-control" id="Password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="form-control btn btn-primary" value="Login" id="username" name="register">
+                    </div>
+                </form>
+            </div>
+
+            <div class="outsideSign-In form-box">
+                <form id="form2"""action="_logincheck.php" method="post">
+                    <div class="text-center">
+                        <h3>Not a Student?</h3>
+                    </div>
+                    <br><br>
+                    <div class="colButton form-group">
+                        <input type="submit" class="form-control btn btn-success" value="Register">
+                        <input type="submit" id="login" class="form-control btn btn-primary" value="Login" id="username" name="login">
+                    </div>
+                    <?php if (isset($_GET['error'])): ?>
+                        <p class="text-center text-danger bg-danger"><?= $_GET['error']; ?></p>
+                    <?php endif; ?>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</body>
+
+</html>
+
+<?php include 'includes/footer.php'; ?>

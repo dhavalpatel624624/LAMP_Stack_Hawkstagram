d78a7b0184af992b401463be998e012ace402f16
JacquelleR <jrowell@hawk.iit.edu>
2017-03-06 20:31:48 -0600

    Touch ups

--- webpages/uploadPic.php
+++ webpages/uploadPic.php
@@ -1,9 +1,9 @@
-<?php include 'includes/header.php'; include 'includes/nav.php'; ?>
+<?php include 'includes/header.php'; ?>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-primary">
-                <div class="panel-heading text-center"><h2 id="photoUpload">Photo Upload</h2></div>
+                <div class="panelHead text-center"><h2 id="photoUpload">Photo Upload</h2></div>
                 <div class="panel-body">
                     <form class="" action="_uploadinstapic.php" method="post" enctype="multipart/form-data">
                         <div class="form-group">
@@ -15,7 +15,7 @@
                             <textarea class="form-control" id="description" name="description" placeholder="Where was this picture taken? What was the name of the event?" rows="8" cols="40"></textarea>
                         </div>
                         .<div class="form-group">
-                            <input type="submit" class="form-control btn btn-success" id="submit" name="upload" value="Upload your photo">
+                            <input type="submit" class="form-control btn btn-brown" id="submit" name="upload" value="Upload your photo">
                         </div>
                     </form>
                 </div>

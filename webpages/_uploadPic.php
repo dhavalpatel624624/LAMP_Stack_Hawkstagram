5d2eef9955ff7ff601054fb27a35998c4cfcf098
JacquelleR <jrowell@hawk.iit.edu>
2017-03-08 10:02:03 -0600

    PHP to upload
    what connects with the file system

--- webpages/_uploadPic.php
+++ webpages/_uploadPic.php
@@ -4,16 +4,30 @@
       $title = $_POST['title'];
       $description = $_POST['description'];
   }
-  $target = "photouploads/";
-  $target = $target. basename($_FILES['photo']['name']);
-
   $tmpname = $_FILES['photo']['tmp_name'];
+  $target = "photouploads/";
+  $target = $target. basename($tmpname);
+  $uploadOk = 1;
+  $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
   move_uploaded_file($tmpname, $target);
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+        echo 'Here is some more debugging info:';
+        print_r($_FILES);
 
-  if (!empty($result)) {
-      header('Location: profile.php');
-  }else {
-      echo "fail";
-  }
-
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($tmpname, $target_file)) {
+                echo "The file " . basename($tmpname) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+    }
 ?>

35eaf8d44477ca07eb75c45372f6615111d68b42
dhavalpatel624624 <dhavalpatel624624@gmail.com>
2017-03-08 01:48:24 -0600

    Landing page for the site

---
+++ webpages/home.php
@@ -0,0 +1,17 @@
+<?php include 'includes/header.php'; ?>
+<head>
+    <title>Hawkstagram Home</title>
+</head>
+<div class="Head">
+    <h1 id="hawkstagram">Hawkstagram</h1>
+</div>
+<div>
+    <?php include 'includes/nav.php'; ?>
+</div>
+<div id="slideshow">
+    <img src="css/images/lifting.jpg" id="slide">
+</div>
+<script src="js/slideshow.js">
+</script>
+
+<?php include 'includes/footer.php';?>

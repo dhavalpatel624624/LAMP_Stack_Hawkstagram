1180311cc7a87ed9df6ff2ff318ae530a300b0a0
JacquelleR <jrowell@hawk.iit.edu>
2017-03-22 10:58:53 -0500

    NavBar
    updated the navagation bar links

--- webpages/includes/nav.php
+++ webpages/includes/nav.php
@@ -1,12 +1,13 @@
 <?php include 'header.php'; ?>
 <?php //$username = $_SESSION['username']; ?>
-<div class="nav">
+<div id="nav">
     <nav id="navigation">
         <ul>
             <li><a href="home.php">Home</a></li>
-            <li><a href="uploadPic.php">Upload Pic</a></li>
-            <li><a href="viewPic.php">View Pics</a></li>
-            <li><a href="index.php">Sign in/Sign up</a></li>
+            <li><a href="about.php">About</a> </li>
+            <!--li><a href="uploadPic.php">Upload Pic</a></li>
+            <li><a href="viewPic.php">View Pics</a></li-->
+            <li><a href="index.php">Sign In</a></li>
         </ul>
     </nav>
 </div>

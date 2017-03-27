140f0db411d5aab5cd0e71a102ef257164eca4f2
drubio687 <drubio1@hawk.iit.edu>
2017-03-27 13:59:23 -0500

    Edited email addresses

---
+++ sql/DummyData.sql
@@ -0,0 +1,110 @@
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('JValenine', 'Jv121@hawk.iit.edu', 'password', 'Jill', 'Valentine',NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('RChambers', 'rcham231@hawk.iit.edu', 'password', 'Rebecca', 'Chambers', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('AWesker', 'awesk123@hawk.iit.edu', 'password', 'Albert', 'Wesker', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('CRedfield', 'cred123@hawk.iit.edu', 'password', 'Chris', 'Redfield', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('EWinters', 'Ewint123@hawk.iit.edu', 'password', 'Ethan', 'Winters', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('BVickers', 'bvick123@hawk.iit.edu', 'password', 'Brad', 'Vickers', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('DVazquez', 'Dvaz123@hawk.iit.edu', 'password', 'Damary', 'Vazquez', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('CRedfield1', 'Cred6146@hawk.iit.edu', 'password', 'Claire', 'Redfield', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('SBurkin', 'Sburkin231@hawk.iit.edu', 'password', 'Sherry', 'Burkin', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('StBurnside', 'stburn@hawk.iit.edu', 'password', 'Steve', 'Burnside', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('Link', 'link123@hawk.iit.edu', 'password', 'Link', 'Hyrule', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('Zelda', 'zelda123@hawk.iit.edu', 'password', 'Zelda', 'Hyrule', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('Ganondorf', 'ganon123@hawk.iit.edu', 'password', 'Ganon', 'Gerudo', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('LKnope', 'leslie123@hawk.iit.edu', 'password', 'Leslie', 'Knope', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('ARose', 'axel123@hawk.iit.edu', 'password', 'Axel', 'Rose', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('SSLash', 'slash123@hawk.iit.edu', 'password', 'Slash', 'Monster', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('DMckaggen', 'Duff123@hawk.iit.edu', 'password', 'Duff', 'Mckaggen', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('AAshford', 'AAshford123@hawk.iit.edu', 'password', 'Alexa', 'Ashford', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('AAshford1', 'AAshford456@hawk.iit.edu', 'password', 'Alfred', 'Ashford', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('KBowyer', 'KBowyer1@hawk.iit.edu', 'password', 'Kevin', 'Bowyer', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('PChan', 'PChan1@hawk.iit.edu', 'password', 'Paulo', 'Chandocha', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('CStrife', 'Cstrife1@hawk.iit.edu', 'password', 'Cloud', 'Strife', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('SqLeonheart', 'SLeon123@hawk.iit.edu', 'password', 'Squall', 'Leonheart', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('TBranford', 'Tbranford1@hawk.iit.edu', 'password', 'Terra', 'Branford', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('CChere', 'Celes23@hawk.iit.edu', 'password', 'Celes', 'Chere', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('ZTail', 'Zidane234@hawk.iit.edu', 'password', 'Zidane', 'Tail', NOW(), NOW());
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('JHetfield', 'JHetfield12@hawk.iit.edu', 'password', 'James', 'Hetfield', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('LUlrcich', 'LUlrich123@hawk.iit.edu', 'password', 'Lars', 'Ulrich', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('KHammett', 'KHammett@hawk.iit.edu', 'password', 'Kirk', 'Hammett', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('RTrujillo', 'RTrujillo2@hawk.iit.edu', 'password', 'Rob', 'Trujillo', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('DMustaine', 'DMustaine@hawk.iit.edu', 'password', 'Dave', 'Mustaine', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('MScott', 'MScott@hawk.iit.edu', 'password', 'Michael', 'Scott', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('JimH', 'JimH4@hawk.iit.edu', 'password', 'Jim', 'Halpert', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('RyanH', 'RyanH34@hawk.iit.edu', 'password', 'Ryan', 'Howard', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('PamB', 'PBeasly@hawk.iit.edu', 'password', 'Pam', 'Beasly', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('GBrady', 'GBrady12@hawk.iit.edu', 'password', 'Greg', 'Brady', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('PBrady', 'PBrady12@hawk.iit.edu', 'password', 'Greg', 'Brady', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('BBrady', 'BBrady1@hawk.iit.edu', 'password', 'Bobby', 'Brady', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('MBrady', 'MBrady@hawk.iit.edu', 'password', 'Marcia', 'Brady', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('JBrady', 'JBrady1@hawk.iit.edu', 'password', 'Jan', 'Brady', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('CBrady', 'CBrady@hawk.iit.edu', 'password', 'Cindy', 'Brady', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('JHowlett', 'JHowlett@hawk.iit.edu', 'password', 'James', 'Howlett', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('OMonroe', 'OMonroe1@hawk.iit.edu', 'password', 'Ororo', 'Monroe', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('BDrake', 'BDrake@hawk.iit.edu', 'password', 'Bobby', 'Drake', NOW(), NOW());
+
+INSERT INTO users (username, email, salted_password, first_name, last_name, date_created, date_updated)
+VALUES ('SSummers', 'SSummers@hawk.iit.edu', 'password', 'Scott', 'Summers', NOW(), NOW());
+
+

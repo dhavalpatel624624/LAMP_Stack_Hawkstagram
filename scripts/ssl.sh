5d0c14369c5d825e2036854d980689b7839b6385
Jason Hedlund <Jason Hedlund>
2017-03-29 12:02:31 -0500

    Getting rid of expect, and trying printf

--- scripts/ssl.sh
+++ scripts/ssl.sh
@@ -2,34 +2,15 @@
 # ssl script
 #
 
-sudo apt-get install -y expect
-
 sudo a2enmod ssl
 
 sudo service apache2 restart
 
 sudo mkdir /etc/apache2/ssl
 
-spawn openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
 
-expect "Country Name (2 letter code) [AU]:"
-send "US"
+printf 'US\Illinois\Chicago\Illinois Institute of Technology\Information Technology Management\Hawkstagram\jhedlund@hawk.iit.edu'
 
-expect "State or Province Name (full name) [Some-State]:"
-send "Illinois"
 
-expect "Locality Name (eg, city) []:"
-send "Chicago"
-
-expect "Organization Name (eg, company) [Internet Widgits Pty Ltd]:"
-send "Illinois Institute of Technology"
-
-expect "Organizational Unit Name (eg, section) []:"
-send "Information Technology Management"
-
-expect "Common Name (e.g. server FQDN or YOUR name) []:"
-send "Hawkstagram"
-
-expect "Email Address []:"
-send "jhedlund@hawk.iit.edu"
 

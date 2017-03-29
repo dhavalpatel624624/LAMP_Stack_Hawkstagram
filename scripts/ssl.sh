518ac15acf703182fe9dcf54803aaa7e9494f8b5
Jason Hedlund <Jason Hedlund>
2017-03-29 12:27:41 -0500

    Fixed syntax errors, and added echo for automation

--- scripts/ssl.sh
+++ scripts/ssl.sh
@@ -8,9 +8,9 @@ sudo service apache2 restart
 
 sudo mkdir /etc/apache2/ssl
 
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
+echo -e 'US\nIllinois\nChicago\nIllinois Institute of Technology\nInformation Technology Management\nHawkstagram\njhedlund@hawk.iit.edu\n'| sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
 
-printf 'US\Illinois\Chicago\Illinois Institute of Technology\Information Technology Management\Hawkstagram\jhedlund@hawk.iit.edu'
+
 
 
 

51aa977426607b22eae9463040759fb495317e95
keunglh <lkeung@hawk.iit.edu>
2017-04-05 11:01:03 -0500

    added full path directory to copy SSL certs

--- scripts/ssl.sh
+++ scripts/ssl.sh
@@ -8,9 +8,9 @@ sudo service apache2 restart
 
 sudo mkdir /etc/apache2/ssl
 
-sudo cp team-2-hawkstagram/scripts/ssl/apache.crt /etc/apache2/ssl
-sudo cp team-2-hawkstagram/scripts/ssl/apache.key /etc/apache2/ssl
-sudo cp team-2-hawkstagram/scripts/ssl/default-ssl.conf /etc/apache2/sites-available/
+sudo cp /var/www/team-2-hawkstagram/scripts/ssl/apache.crt /etc/apache2/ssl
+sudo cp /var/www/team-2-hawkstagram/scripts/ssl/apache.key /etc/apache2/ssl
+sudo cp /var/www/team-2-hawkstagram/scripts/ssl/default-ssl.conf /etc/apache2/sites-available/
 
 sudo a2ensite default-ssl.conf
 

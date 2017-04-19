8f822f5964a9ace00c9a5abff2c9a7e410ff52bd
drubio687 <drubio1@hawk.iit.edu>
2017-04-19 11:11:13 -0500

    Added dummy data to photo tables

---
+++ sql/photodata.sql
@@ -0,0 +1,23 @@
+INSERT INTO photos (user_id, caption, image_path, image_size, date_created)
+VALUES (1, 'First Picture','pic1.jpg', 23, NOW());
+
+INSERT INTO photos (user_id, caption, image_path, image_size, date_created)
+VALUES (3, 'Second Picture','pic1.jpg', 23, NOW());
+
+INSERT INTO photos (user_id, caption, image_path, image_size, date_created)
+VALUES (5, 'Cool pic','pic1.jpg', 23, NOW());
+
+INSERT INTO photos (user_id, caption, image_path, image_size, date_created)
+VALUES (10, '#weekendshots','pic1.jpg', 23, NOW());
+
+INSERT INTO photos (user_id, caption, image_path, image_size, date_created)
+VALUES (8, 'Bffs','pic1.jpg', 23, NOW());
+
+INSERT INTO photos (user_id, caption, image_path, image_size, date_created)
+VALUES (7, '#caseofthemondays','pic1.jpg', 23, NOW());
+
+INSERT INTO photos (user_id, caption, image_path, image_size, date_created)
+VALUES (5, 'awesome','pic1.jpg', 23, NOW());
+
+INSERT INTO photos (user_id, caption, image_path, image_size, date_created)
+VALUES (2, 'tubular','pic1.jpg', 23, NOW());

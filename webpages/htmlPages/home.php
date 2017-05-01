ed1b1e6eaaf1c664fa5df0cf5557033de860c2fd
JacquelleR <jrowell@hawk.iit.edu>
2017-05-01 16:25:18 -0500

    Changed Slideshow
    Slideshow is now the entire backgound (using WOW slider for cleaner JS)
    and features Commencement picture instead of dumby data

---
+++ webpages/htmlPages/home.php
@@ -0,0 +1,60 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hawkstagram</title>
+    <link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <link rel="stylesheet" type="text/css" href="css/navLoggedOut.css">
+
+    <!-- Start WOWSlider.com HEAD section -->
+    <link rel="stylesheet" type="text/css" href="js/engine1/style.css" />
+    <script type="text/javascript" src="js/engine1/jquery.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/ws-bg.css" />
+    <!-- End WOWSlider.com HEAD section -->
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="Head">
+                <h1 id="hawkstagram">Hawkstagram</h1>
+            </div>
+            <?php include 'includes/nav.php'?>
+            <div class="overlay">
+                <div id="text">
+                    <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog.
+                        Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs.
+                        Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz.
+                        Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack.
+                        Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim.
+                        Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex.
+                        Two driven jocks help fax my big quiz.Quick, Baz, get my woven flax jodhpurs!
+                        "Now fax quiz Jack!" my brave ghost pled.Five quacking zephyrs jolt my wax bed.
+                        Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves quart jug of bad milk.
+                        A very bad quack might jinx zippy fowls. Few quips galvanized the mock jury box.
+                        Quick brown dogs jump over the lazy fox. The jay, pig, fox, zebra, and my wolves quack!
+                        Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck.
+                        A wizard’s job is to vex chumps quickly in fog. Watch "Jeopardy!", Alex Trebek's fun TV quiz game.
+                    </p>
+                </div>
+            </div>
+            <!-- Start WOWSlider.com BODY section -->
+            <div id="wowslider-container1">
+                <div class="ws_images"><ul>
+                        <li><img src="js/data1/images/09commencement_main_019.jpg" alt="09commencement_main_019" title="09commencement_main_019" id="wows1_0"/></li>
+                        <li><img src="js/data1/images/09commencement_main_044.jpg" alt="09commencement_main_044" title="09commencement_main_044" id="wows1_1"/></li>
+                        <li><img src="js/data1/images/09commencement_main_045.jpg" alt="09commencement_main_045" title="09commencement_main_045" id="wows1_2"/></li>
+                        <li><img src="js/data1/images/09commencement_main_039.jpg" alt="responsive slider" title="09commencement_main_039" id="wows1_3"/></a></li>
+                        <li><img src="js/data1/images/09commencement_main_008.jpg" alt="09commencement_main_008" title="09commencement_main_008" id="wows1_4"/></li>
+                    </ul></div>
+                <div class="ws_shadow"></div>
+            </div>
+            <script type="text/javascript" src="js/engine1/wowslider.js"></script>
+            <script type="text/javascript" src="js/engine1/script.js"></script>
+            <!-- End WOWSlider.com BODY section -->
+        </div>
+    </div>
+    <?php include 'includes/footer.php';?>

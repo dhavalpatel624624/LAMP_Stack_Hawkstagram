5c5d50165105eadc084555e9319491d8023f73be
JacquelleR <jrowell@hawk.iit.edu>
2017-05-02 08:59:39 -0500

    Gallery Function added

--- webpages/viewPic.php
+++ webpages/viewPic.php
@@ -1,4 +1,4 @@
-<?php include 'includes/header.php';
+<?php include_once('galleryFrame/resources/UberGallery.php');
 
 //$path_parts = pathinfo($_GET['file']);
 $file_name  = 'Capture.PNG';
@@ -6,37 +6,48 @@ $file_path  = './photouploads/' . $file_name;
 file_get_contents($file_path);
 
 ?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <!--?php while($row = mysqli_fetch_object($result)): ?-->
-            <div class="panel panel-info">
-                <div class="panel-heading text-center"><h4><!--?= $row -> title ?--></h4></div>
-                <div class="panel-body">
-                    <div class="col-md-12">
-                        <?php
-                            echo '<img src="' . htmlspecialchars($file_path) . '"/>';
-                        ?>
-                    </div>
-                </div>
-                <!-- php $userid = $row -> user_id; ?>
-             <!?php endwhile; ?-->
-                <div class="panel-footer">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <form  action="viewPic.php" method="post">
-                                <div class="form-group">
-                                    <label for="photo" class="control-label">Choose your photo:</label>
-                                    <textarea class="form-control" id="description" name="description" placeholder="Enter Description of your Insta pic" rows="8" cols="40"></textarea>
-                                </div>
-
-                                <input type="submit" class="form-control btn btn-red" value="Download" >
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/navLoggedOut.css">
+    <link rel="stylesheet" type="text/css" href="gallaryFrame/resources/colorbox/1/colorbox.css" />
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/rebase-min.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"></script>
+    <script type="text/javascript" src="resources/colorbox/jquery.colorbox.js"></script>
+    <title>Hawkstagram</title>
+</head>
+<body>
+<div class="Head">
+    <h1 id="hawkstagram">Hawkstagram</h1>
 </div>
+<?php include 'includes/nav.php'?>
+<div class="container">
+
+    <?php $files = scandir('galleryFrame/galleries'); ?>
+    <?php foreach ($files as $file): ?>
+
+        <?php $dir = 'galleryFrame/galleries/' . $file; ?>
+
+        <?php if (is_dir($dir) && $file != '.' && $file != '..'): ?>
+            <h2 class="gallery"><?php echo ucwords($file); ?></h2>
+            <?php $gallery = UberGallery::init()->createGallery($dir, $file); ?>
+        <?php endif; ?>
+
+    <?php endforeach; ?>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("a[rel='graduation']").colorbox({maxWidth: "90%", maxHeight: "90%", opacity: ".5"});
+        $("a[rel='luncheon']").colorbox({maxWidth: "90%", maxHeight: "90%", opacity: ".5"});
+        $("a[rel='misc']").colorbox({maxWidth: "90%", maxHeight: "90%", opacity: ".5"});
+    });
+</script>
 <?php include 'includes/footer.php'; ?>
